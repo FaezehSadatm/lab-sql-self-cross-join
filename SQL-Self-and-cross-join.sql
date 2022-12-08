5711400b218cc8e6e6e7d55babc8369b86d8d7b6
FaezehSadat <faezehsadatm@gmail.com>
2022-12-09 00:12:00 +0100

    correction

--- SQL-Self-and-cross-join.sql
+++ SQL-Self-and-cross-join.sql
@@ -9,6 +9,25 @@ and fa1.film_id = fa2.film_id
 join actor a2 on fa2.actor_id = a2.actor_id;
 
 #2. Get all pairs of customers that have rented the same film more than 3 times.
+select r1.customer_id, r2.customer_id, r2.film_id from (
+	select r.customer_id, i.film_id, count(r.rental_id)
+    from rental r
+    join inventory i on r.inventory_id = i.inventory_id
+    group by r.customer_id, i.film_id
+    having count(r.rental_id) > 1
+    ) as r1
+join (
+	select r.customer_id, i.film_id, count(r.rental_id)
+    from rental r
+	join inventory i on r.inventory_id = i.inventory_id
+	group by r.customer_id, i.film_id
+	having count(r.rental_id) > 1
+    ) as r2
+on r1.film_id = r2.film_id
+and r1.customer_id > r2.customer_id;
+ 
+-- or
+
 select r1.customer_id, count(r1.customer_id) over (partition by i1.film_id) ,i1.film_id, r2.customer_id, count(r2.customer_id) over (partition by i1.film_id)
 from rental r1
 join inventory i1 on r1.inventory_id = i1.inventory_id
@@ -17,6 +36,7 @@ on i1.film_id = i2.film_id
 join rental r2 on i2.inventory_id = r2.inventory_id
 where r1.customer_id <> r2.customer_id
 order by r1.customer_id;
+
 
 #3. Get all possible pairs of actors and films.
 select * from film f
